076efa14dd3516239826a3e06f9f142248d01b82
chelseasoemitro <chelseasoemitro@gmail.com>
2025-05-08 19:56:46 -0400

    fix Makefile and rename output file

--- Makefile
+++ Makefile
@@ -1,20 +1,12 @@
 OCB=ocamlbuild -use-ocamlfind -pkg llvm
 TARGET=capybara.native
 
-.PHONY: all tests clean run_example1 run_example2
+.PHONY: all tests clean
 
 all: $(TARGET)
 
 $(TARGET):
 	$(OCB) $(TARGET)
-
-run_example1:
-	./capybara.native -c example.cap
-	lli a.out
-
-run_example2:
-	./capybara.native -c example2.cap
-	lli a.out
 
 clean:
 	$(OCB) -clean
--- capybara.ml
+++ capybara.ml
@@ -11,11 +11,17 @@
        ("-a", Arg.Unit (set_action Ast), "Print the AST");
        ("-s", Arg.Unit (set_action Sast), "Print the SAST");
        ("-l", Arg.Unit (set_action LLVM_IR), "Print the generated LLVM IR");
-       ("-c", Arg.Unit (set_action Compile), "Dump the compiled program to a.out")
+       ("-c", Arg.Unit (set_action Compile), "Dump the compiled program to the executable file")
      ] in
      let usage_msg = "usage: ./capybara.native [-a|-s|-l|-c] [file.cap]" in
      let channel = ref stdin in
-     Arg.parse speclist (fun filename -> channel := open_in filename) usage_msg;
+     let filename = ref "" in
+
+     (* Parsing the command-line arguments *)
+     Arg.parse speclist (fun fname -> 
+       channel := open_in fname; 
+       filename := fname
+     ) usage_msg;
    
      let lexbuf = Lexing.from_channel !channel in
    
@@ -28,6 +34,8 @@
        | Sast    -> print_string (Sast.string_of_sprogram sast)
        | LLVM_IR -> print_string (Llvm.string_of_llmodule (Codegen.translate sast))
        | Compile -> 
-          let oc = open_out "a.out" in
+          let output_filename = Filename.remove_extension !filename in
+          (* open the output file and write the LLVM IR *)
+          let oc = open_out output_filename in
           output_string oc (Llvm.string_of_llmodule (Codegen.translate sast));
           close_out oc;
